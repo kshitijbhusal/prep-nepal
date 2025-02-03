a406793fdaac065a9bae75cfd94f05d76b985ec3
Kshitij Bhusal <kshitijbhusal64@gmail.com>
2025-02-03 22:10:14 +0545

    env setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-export const connection = mongoose
-  .connect("mongodb://0.0.0.0/prepnepal")
-  .then(() => {
-    console.log("Connected to Data Base");
-  });
+export const connection = mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("Connected to Data Base");
+});
